e4f02728181bc9927dac318182f0e090a5bbdeba
Eric MacDonald <ericmacdonald@Erics-MacBook-Pro.local>
2018-01-04 13:15:18 -0500

    working, ready to submit

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,59 +1,46 @@
+//establish some gloal variables
 var wins = 0;
 var losses = 0;
 var total_score = 0;
+var target_number;
+var buttonArray=[];
 
+//reset everything...probably unnecessary
 reset_game();
 
 
-$(".gem_1").on("click", function() {
-	total_score = total_score + button_1;
+//anytime a gem is clicked, run this
+$(".gem").on("click", function() {
+	var indexValue = $(this).attr("value");
+	total_score += buttonArray[indexValue];
 	$(".score").text(total_score);
 	check_results();
 });
 
-$(".gem_2").on("click", function() {
-	total_score = total_score + button_2;
-	$(".score").text(total_score);
-	check_results();
-
-});
-
-$(".gem_3").on("click", function() {
-	total_score = total_score + button_3;
-	$(".score").text(total_score);
-	check_results();
-});
-
-$(".gem_4").on("click", function() {
-	total_score = total_score + button_4;
-	$(".score").text(total_score);
-	check_results();
-});
-
+//check to see if we are on or over the target
 function check_results() {
-	if (total_score == target_number) {
-		wins++;
-		reset_game();
-		alert ("you win!");
-	}
-	else if (total_score > target_number) {
-		losses++;
-		reset_game();
-		alert ("you lose!");
-	}
+	setTimeout(function() {
+		if (total_score == target_number) {
+				wins++;
+				alert ("you win!");
+				reset_game();
+		}
+		else if (total_score > target_number) {
+			losses++;
+			alert ("you lose!");
+			reset_game();
+		}
+	}, 200);
 }
 
+//clears everything, sets random values for target and gems
 function reset_game(){
-		target_number = Math.floor((Math.random() * 40) + 1);
-		button_1 = Math.floor((Math.random() * 10) + 1);
-		button_2 = Math.floor((Math.random() * 10) + 1);
-		button_3 = Math.floor((Math.random() * 10) + 1);
-		button_4 = Math.floor((Math.random() * 10) + 1);
+		target_number = Math.floor((Math.random() * 40) + 10);
+		for (i=0; i<=3; i++) {
+			buttonArray[i]=Math.floor((Math.random() * 10) + 1);
+		}
 		console.log("target_number: " + target_number);
-		console.log("button_1: " + button_1);
-		console.log("button_2: " + button_2);
-		console.log("button_3: " + button_3);
-		console.log("button_4: " + button_4);
+		console.log("buttonArray: " + buttonArray);
 		total_score = 0;
 		$(".score").text(total_score);
 		$(".target").text(target_number);
