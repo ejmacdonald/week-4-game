dbb8ef841f83faaed722c38444b597de474a4f13
Eric MacDonald <ericmacdonald@Erics-MacBook-Pro.local>
2018-01-06 10:36:18 -0500

    removed hoisting

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,17 +5,19 @@ var total_score = 0;
 var target_number;
 var buttonArray=[];
 
-//reset everything...probably unnecessary
-reset_game();
+//clears everything, sets random values for target and gems
+function reset_game(){
+		target_number = Math.floor((Math.random() * 102) + 19);
+		for (i=0; i<=3; i++) {
+			buttonArray[i]=Math.floor((Math.random() * 12) + 1);
+		}
+		total_score = 0;
+		$(".score").text(total_score);
+		$(".target").text(target_number);
+		$(".wins").text(wins);
+		$(".losses").text(losses);
 
-
-//anytime a gem is clicked, run this
-$(".gem").on("click", function() {
-	var indexValue = $(this).attr("value");
-	total_score += buttonArray[indexValue];
-	$(".score").text(total_score);
-	check_results();
-});
+}
 
 //check to see if we are on or over the target
 function check_results() {
@@ -33,18 +35,18 @@ function check_results() {
 	}, 200);
 }
 
-//clears everything, sets random values for target and gems
-function reset_game(){
-		target_number = Math.floor((Math.random() * 102) + 19);
-		for (i=0; i<=3; i++) {
-			buttonArray[i]=Math.floor((Math.random() * 12) + 1);
-		}
-		console.log("target_number: " + target_number);
-		console.log("buttonArray: " + buttonArray);
-		total_score = 0;
-		$(".score").text(total_score);
-		$(".target").text(target_number);
-		$(".wins").text(wins);
-		$(".losses").text(losses);
+//reset everything...probably unnecessary
+reset_game();
 
-}
+
+
+//anytime a gem is clicked, run this
+$(".gem").on("click", function() {
+	var indexValue = $(this).attr("value");
+	total_score += buttonArray[indexValue];
+	$(".score").text(total_score);
+	check_results();
+});
+
+
+
